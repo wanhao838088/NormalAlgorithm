5fe9df9bf47f8d015eb44d68a01e97740b3fe1ff
wanhao <838088516@qq.com>
2019-06-27 09:12:20 +0800

    二叉树线索化

---
+++ src/tree/BinSearchTree.java
@@ -0,0 +1,360 @@
+package tree;
+
+import java.util.LinkedList;
+
+/**
+ * Created by LiuLiHao on 2019/5/18 0018 下午 07:22
+ * @author : LiuLiHao
+ * 描述：二分搜索树
+ */
+public class BinSearchTree<K extends Comparable<K>,V> {
+
+    private int count;
+    private Node<K,V> root;
+
+    /**
+     * 保存前一次遍历内容
+     */
+    private Node<K,V> pre;
+
+    static class Node<K,V>{
+        private K key;
+        private V value;
+        public Node<K,V> left;
+        public Node<K,V> right;
+        public int leftType;
+        public int rightType;
+
+
+        public Node(K key,V value) {
+            this.key = key;
+            this.value = value;
+            this.left = this.right = null;
+        }
+        public Node(Node<K,V> node){
+            this.key = node.key;
+            this.value = node.value;
+            this.left = node.left;
+            this.right = node.right;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    ", leftType=" + leftType +
+                    ", rightType=" + rightType +
+                    '}';
+        }
+    }
+
+    /**
+     * 创建二分搜索树
+     */
+    public BinSearchTree() {
+        this.count = 0;
+        this.root = null;
+    }
+
+
+    /**
+     * 插入元素
+     * @param k
+     * @param v
+     */
+    public void insert(K k,V v){
+        this.root = insert(root,k,v);
+    }
+
+
+    /**
+     * 最小的元素
+     * @return
+     */
+    public K min(){
+        Node<K,V> node = min(root);
+        return node.key;
+    }
+
+    private Node<K,V> min(Node<K, V> node) {
+        if (node.left==null){
+            return node;
+        }
+
+        return min(node.left);
+    }
+
+    /**
+     * 删除任意元素
+     * @param k
+     */
+    public void remove(K k){
+        root = remove(root,k);
+    }
+
+    private Node<K, V> remove(Node<K, V> node, K k) {
+        if (node==null){
+            return null;
+        }else if(k.compareTo(node.key)<0){
+            //从左边删除
+            node.left = remove(node.left,k);
+            return node;
+        }else if(k.compareTo(node.key)>0){
+            //从右边删除
+            node.right = remove(node.right,k);
+            return node;
+        }else {
+
+            if (node.left==null){
+                //左边为空
+                Node<K, V> right = node.right;
+                count--;
+                return right;
+            }
+            if (node.right==null){
+                //右边为空
+                Node<K, V> left = node.left;
+                count--;
+                return left;
+            }
+            //选择右边最小的元素为代替节点
+            Node<K, V> min = new Node<>(min(node.right));
+            count++;
+            min.right = removeMin(node);
+            min.left = node.left;
+            node.left = node.right = null;
+            count--;
+            return min;
+        }
+    }
+
+    /**
+     * 删除最小元素
+     */
+    public void removeMin(){
+        root = removeMin(root);
+    }
+
+    private Node<K, V> removeMin(Node<K, V> node) {
+        if (node.left==null){
+            //删除这个节点
+            Node<K, V> right = node.right;
+            node.right = null;
+            count--;
+            return right;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    /**
+     * 删除最大元素
+     */
+    public void removeMax(){
+        root = removeMax(root);
+    }
+
+    private Node<K, V> removeMax(Node<K, V> node) {
+        if (node.right==null){
+            //删除节点
+            Node<K, V> left = node.left;
+            node.left = null;
+            count--;
+            return left;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
+    /**
+     * 最大元素
+     * @return
+     */
+    public K max(){
+        Node<K,V> node = max(root);
+        return node.key;
+    }
+
+    private Node<K,V> max(Node<K, V> node) {
+        if (node.right==null){
+            return node;
+        }
+        return max(node.right);
+    }
+
+    /**
+     * 是否包含这个key
+     * @param k
+     * @return
+     */
+    public boolean contain(K k){
+        return contain(root,k);
+    }
+
+    private boolean contain(Node<K, V> node, K k) {
+        if (node==null){
+            return false;
+        }else if(k.compareTo(node.key)<0){
+            return contain(node.left,k);
+        }else {
+            return contain(node.right,k);
+        }
+    }
+
+    /**
+     * 使用key查找元素的值
+     * @param k
+     * @return
+     */
+    public V search(K k){
+        return search(root,k);
+    }
+
+    private V search(Node<K, V> node, K k) {
+        if (node==null){
+            return null;
+        }
+        //找到了
+        if (k==node.key){
+            return node.value;
+        } else if(k.compareTo(node.key)<0){
+            return search(node.left,k);
+        }else {
+            return search(node.right,k);
+        }
+    }
+
+
+    private Node insert(Node<K, V> node, K k, V v) {
+        //递归结束
+        if (node==null){
+            count++;
+            return new Node<>(k, v);
+        }
+        //比较元素和节点的大小
+        if (k.compareTo(root.key)==0){
+            //相同则更新
+            node.value = v;
+        }else if (k.compareTo(node.key)<0){
+            //k小于  往左放
+            node.left = insert(node.left,k,v);
+        }else {
+            //k大于 往右放
+            node.right = insert(node.right,k,v);
+        }
+        return node;
+    }
+
+    /**
+     * 广度优先遍历树
+     */
+    public void orderTree(){
+        LinkedList<Node<K, V>> list = new LinkedList<>();
+        list.addFirst(root);
+        while (!list.isEmpty()){
+            Node<K, V> node = list.pop();
+            System.out.println(node.key);
+            //如果有子节点 继续添加遍历
+            if (node.left!=null){
+                list.push(node.left);
+            }
+            if (node.right!=null){
+                list.push(node.right);
+            }
+        }
+    }
+
+    /**
+     * 前序遍历
+     */
+    public void preOrder(){
+        preOrder(root);
+    }
+
+    private void preOrder(Node<K, V> node) {
+        if (node!=null){
+            System.out.println(node.key);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    /**
+     * 中序遍历
+     */
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    private void inOrder(Node<K, V> node) {
+        if (node!=null){
+            inOrder(node.left);
+            System.out.println(node.key);
+            inOrder(node.right);
+        }
+    }
+
+    public void postOrder(){
+        postOrder(root);
+    }
+
+    private void postOrder(Node<K, V> node) {
+        if (node!=null){
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.println(node.key);
+        }
+    }
+    /**
+     * 元素个数
+     * @return
+     */
+    public int size(){
+        return count;
+    }
+
+    /**
+     * 是否为空
+     * @return
+     */
+    public boolean isEmpty(){
+        return count==0;
+    }
+
+    /**
+     * 中序线索化
+     */
+    public void midThreaded(){
+        midThreaded(root);
+    }
+
+    /**
+     * 中序线索化
+     */
+    private void midThreaded(Node<K,V> node){
+        if (node==null){
+            return;
+        }
+        //左侧线索化
+        midThreaded(node.left);
+
+        if (node.left==null){
+            node.left = pre;
+            node.leftType = 1;
+        }
+        if (pre!=null && pre.right==null){
+            pre.right = node;
+            pre.rightType = 1;
+        }
+        pre = node;
+
+        //右侧线索化
+        midThreaded(node.right);
+
+    }
+
+    public void setRoot(Node<K, V> root) {
+        this.root = root;
+    }
+}
--- src/tree/TestArrBinTree.java
+++ src/tree/TestArrBinTree.java
@@ -1,5 +1,7 @@
 package tree;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * Created by LiuLiHao on 2019/6/26 0026 下午 07:33
  * 描述：
@@ -7,6 +9,7 @@ package tree;
 public class TestArrBinTree {
 
     public static void main(String[] args) {
+        //用树的方式遍历数组
         int[] arr = {1,2,3,4,5,6,7};
         ArrBinTree tree = new ArrBinTree(arr);
         tree.preOrder();
@@ -14,5 +17,38 @@ public class TestArrBinTree {
         tree.midOrder();
         System.out.println("-------------");
         tree.postOrder();
+    }
+
+
+    /**
+     * 测试树线索化
+     */
+    @Test
+    public void testThreaded(){
+        BinSearchTree<String, Integer> tree = new BinSearchTree<>();
+
+        BinSearchTree.Node root = new BinSearchTree.Node("1",3);
+        BinSearchTree.Node node2 = new BinSearchTree.Node("3",3);
+        BinSearchTree.Node node3 = new BinSearchTree.Node("6",3);
+        BinSearchTree.Node node4 = new BinSearchTree.Node("8",3);
+        BinSearchTree.Node node5 = new BinSearchTree.Node("10",31);
+        BinSearchTree.Node node6 = new BinSearchTree.Node("14",3);
+
+        root.left = node2;
+        root.right=node3;
+        node2.left=node4;
+        node2.right=node5;
+        node3.left=node6;
+
+        BinSearchTree<String, Integer> threadedBinaryTree = new BinSearchTree<String, Integer>();
+        threadedBinaryTree.setRoot(root);
+        threadedBinaryTree.midThreaded();
+
+        BinSearchTree.Node leftNode = node5.left;
+        BinSearchTree.Node rightNode = node5.right;
+
+        System.out.println("10号结点的前驱结点是 ="  + leftNode); //3
+        System.out.println("10号结点的后继结点是 ="  + rightNode); //1
+
     }
 }
